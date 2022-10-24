34c7e76b3cd979de5a487c1fd89c7877b9ee24d8
Thomas DUCLOS <thomasduclos1012@gmail.com>
2022-10-24 12:25:53 +0900

    iter method and change inside delete head

--- inc/linkedlist.h
+++ inc/linkedlist.h
@@ -18,8 +18,9 @@ linkedList* linkedList_insert_last(linkedList* head, linkedList* elem);
 linkedList* linkedList_insert_after(linkedList* head, linkedList* elem);
 void* linkedList_delete_last(linkedList* head);
 void* linkedList_delete_after(linkedList* head);
-linkedList* linkedList_delete_head(linkedList* head);
+linkedList* linkedList_delete_head(linkedList* head, void* data);
 void linkedList_free(linkedList* head);
 linkedList* linkedList_create_from_list(void* arr, int len, size_t type_size);
+void linkedList_iter_data(linkedList* head, void (*do_)(void*));
 
 #endif
